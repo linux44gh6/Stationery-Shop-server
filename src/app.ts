d0e80ce59e60f2e97965bdf362cf721efc42e9a4
Md Milon <abireshan32@gmail.com>
2024-12-12 17:49:01 +0600

    add some change

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { Request, Response } from 'express'
+// import { Request, Response } from 'express'
 import productRouter from './modules/product/prodcut.router'
 import orderRoute from './modules/order/order.route'
 const app = express()
@@ -7,9 +7,4 @@ const app = express()
 app.use(express.json())
 app.use('/api/products', productRouter)
 app.use('/api/orders', orderRoute)
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
-
 export default app
--- src/modules/order/order.route.ts
+++ src/modules/order/order.route.ts
@@ -2,6 +2,6 @@ import { Router } from 'express'
 import { orderController } from './order.controller'
 const orderRoute = Router()
 
-orderRoute.post('/create-order', orderController.createOrder)
+orderRoute.post('/', orderController.createOrder)
 orderRoute.get('/revenue', orderController.getAllOrders)
 export default orderRoute
